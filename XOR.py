90971b5a1f8e357e3409dc1fa5032a063a637b51
CorentinRmg <60092674+CorentinRmg@users.noreply.github.com>
2020-01-28 13:16:58 +0100

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -24,7 +24,7 @@ def train_xor(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 200
 #train_xor(net1, X, Ytarget)_
 
 
-##NON-LINEAR NETWORK (adding an )
+##NON-LINEAR NETWORK (adding a non-linear activation layer: Tanh(), Sigmoid(), ...)
 net2 = NeuralNet([
         # Add the layers here
         Linear(input_size=2, output_size=2),
