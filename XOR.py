52fe484b83330c8578da9585fe8e2dd7acd26a61
CorentinRmg <60092674+CorentinRmg@users.noreply.github.com>
2020-01-31 16:10:37 +0100

    Update and comment XOR.py
    *Add a feature to resize automatically the NN to the dimensions of the given imputs and targets.
    *Add of comments

--- XOR.py
+++ XOR.py
@@ -18,18 +18,17 @@ Ytarget = np.array([[0], [1], [1], [0]])
 #                    [0, 1],
 #                    [1, 0]])
 
-#X = np.array([[-354, 342]])
-#
-#Ytarget = np.array([[42]])
-
 def print_xor_results(inputs: Tensor, targets: Tensor, predictions: Tensor) -> None:
     print('\n  X   => y_target =>   y_pred  => round(y_pred)')
     for x, y, z in zip(inputs, targets, predictions):
         print(f'{x} =>   {y}    => {z.round(5)} =>  {z.round()}')
         
 def train_xor(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 10000):
+    #trains the Neural Network using the given input and target
     train(net, inputs, targets, num_epochs=epochs)
+    #Uses this trained Neural Network to predict the output with the same given input
     predictions = net.forward(inputs)
+    #prints the results to compare with the expected output
     print_xor_results(inputs, targets, predictions)
 
 
@@ -38,13 +37,20 @@ def train_xor(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 100
 #train_xor(net1, X, Ytarget)_
 
 ##NON-LINEAR NETWORK (adding a non-linear activation layer: Tanh(), Sigmoid(), ...)
+
+#Takes the dimensions of the inputs and outputs to size the NN
+L_in=X.shape[1]
+L_out=Ytarget.shape[1]
+
+#Creation of the Neural Network
 net2 = NeuralNet([
         # Add the layers here
-        Linear(input_size=2, output_size=2),
+        Linear(input_size=L_in, output_size=L_in),
         Tanh(),
         #ReLu(),
         #Sigmoid(),
-        Linear(input_size=2, output_size=1)
+        Linear(input_size=L_in, output_size=L_out)
 ])
 
+#Use of this NN to construct the XOR function
 train_xor(net2, X, Ytarget)
