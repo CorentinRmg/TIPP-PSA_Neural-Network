4787de1aad9473f9449fe25d10e16a4826b01971
CorentinRmg <60092674+CorentinRmg@users.noreply.github.com>
2020-02-12 10:16:12 +0100

    Adds in train_NN
    Add the possibility to choose another loss function in the train_NN function, this also allows to print the final loss function after the outputs.

--- DataBelle2.py
+++ DataBelle2.py
@@ -14,11 +14,11 @@ hMT4=np.array(data['harmonicMomentThrust4'],dtype=float)
 fWR2=np.array(data['foxWolframR2'],dtype=float)
 fWR3=np.array(data['foxWolframR3'],dtype=float)
 
-X=(np.array([ norm(hMT2), norm(hMT4) ])).T
+X=(np.array([ norm(fWR2), norm(hMT2), norm(hMT4) ])).T
 Ytarget=(np.array([np.array(data['isBB'],dtype=float)])).T
 
 #Other data to test the trained NN
-data2=csv2dict("../Data/data200.csv")
+data2=csv2dict("../Data/data100_2.csv")
 
 sph_bis=np.array(data2['sphericity'],dtype=float)
 hMT2_bis=np.array(data2['harmonicMomentThrust2'],dtype=float)
@@ -27,7 +27,7 @@ hMT4_bis=np.array(data2['harmonicMomentThrust4'],dtype=float)
 fWR2_bis=np.array(data2['foxWolframR2'],dtype=float)
 fWR3_bis=np.array(data2['foxWolframR3'],dtype=float)
 
-X2=(np.array([ norm(hMT2_bis), norm(hMT4_bis) ])).T
+X2=(np.array([ norm(fWR2_bis), norm(hMT2_bis), norm(hMT4_bis) ])).T
 Ytarget2=(np.array([np.array(data2['isBB'],dtype=float)])).T
 
 
@@ -38,16 +38,19 @@ def print_results(inputs: Tensor, targets: Tensor, predictions: Tensor) -> None:
     for x, y, z in zip(inputs, targets, predictions):
         print(f'{x} =>   {y}    => {z.round(5)} =>  {z.round()}') #{float(z):.5f} =>    {z.round()}')
         
-def train_NN(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 1000):
-    train(net, inputs, targets, num_epochs=epochs)
+def train_NN(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 1000, loss_func: Loss = MeanSquareError() ):
+    train(net, inputs, targets, num_epochs=epochs, loss=loss_func)
     predictions = net.forward(inputs)
     print_results(inputs, targets, predictions)
+    
+    Loss_value=loss_func.loss(predictions,targets)
+    print("\nloss function:", Loss_value)
 
 def test_NN(net: Optimizer, inputs: Tensor, targets: Tensor, loss_func: Loss = MeanSquareError()):
     predictions = net.forward(inputs)
     print_results(inputs, targets, predictions)    
     Loss_value=loss_func.loss(predictions,targets)
-    print("\nFonction de coût:", Loss_value)
+    print("\nloss function:", Loss_value)
 
 ###MAIN PROGRAM_________________________________________________________________________________________
 
@@ -55,7 +58,7 @@ def test_NN(net: Optimizer, inputs: Tensor, targets: Tensor, loss_func: Loss = M
 L_in=X.shape[1]
 L_out=Ytarget.shape[1]
 
-net2 = NeuralNet([
+net = NeuralNet([
         # Add the layers here
         Linear(input_size=L_in, output_size=L_in),
         Tanh(),
@@ -64,6 +67,6 @@ net2 = NeuralNet([
         Linear(input_size=L_in, output_size=L_out)
 ])
 
-train_NN(net2, X, Ytarget)
-print("\n\n\n")
-test_NN(net2, X2, Ytarget2)
+train_NN(net, X, Ytarget)
+#print("\n\n\n")
+#test_NN(net, X2, Ytarget2)
