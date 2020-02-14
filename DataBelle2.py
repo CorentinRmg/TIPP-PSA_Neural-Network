87229634d5b36b88e0ca963ad6273be3414110c1
CorentinRmg <60092674+CorentinRmg@users.noreply.github.com>
2020-02-14 10:07:50 +0100

    Udpate DataBelle2.py
    New data inputs and some minor changes

--- DataBelle2.py
+++ DataBelle2.py
@@ -13,22 +13,26 @@ hMT3=np.array(data['harmonicMomentThrust3'],dtype=float)
 hMT4=np.array(data['harmonicMomentThrust4'],dtype=float)
 fWR2=np.array(data['foxWolframR2'],dtype=float)
 fWR3=np.array(data['foxWolframR3'],dtype=float)
+cCT0=np.array(data['cleoConeThrust0'],dtype=float)
+cCT1=np.array(data['cleoConeThrust1'],dtype=float)
 
-X=(np.array([ norm(fWR2), norm(hMT2), norm(hMT4) ])).T
+X=(np.array([ norm(fWR2), norm(cCT0), norm(cCT1) ])).T
 Ytarget=(np.array([np.array(data['isBB'],dtype=float)])).T
 
 #Other data to test the trained NN
-data2=csv2dict("../Data/data100_2.csv")
+databis=csv2dict("../Data/data100_2.csv")
 
-sph_bis=np.array(data2['sphericity'],dtype=float)
-hMT2_bis=np.array(data2['harmonicMomentThrust2'],dtype=float)
-hMT3_bis=np.array(data2['harmonicMomentThrust3'],dtype=float)
-hMT4_bis=np.array(data2['harmonicMomentThrust4'],dtype=float)
-fWR2_bis=np.array(data2['foxWolframR2'],dtype=float)
-fWR3_bis=np.array(data2['foxWolframR3'],dtype=float)
+sph_bis=np.array(databis['sphericity'],dtype=float)
+hMT2_bis=np.array(databis['harmonicMomentThrust2'],dtype=float)
+hMT3_bis=np.array(databis['harmonicMomentThrust3'],dtype=float)
+hMT4_bis=np.array(databis['harmonicMomentThrust4'],dtype=float)
+fWR2_bis=np.array(databis['foxWolframR2'],dtype=float)
+fWR3_bis=np.array(databis['foxWolframR3'],dtype=float)
+cCT0_bis=np.array(databis['cleoConeThrust0'],dtype=float)
+cCT1_bis=np.array(databis['cleoConeThrust1'],dtype=float)
 
-X2=(np.array([ norm(fWR2_bis), norm(hMT2_bis), norm(hMT4_bis) ])).T
-Ytarget2=(np.array([np.array(data2['isBB'],dtype=float)])).T
+X_bis=(np.array([ norm(fWR2_bis), norm(cCT0_bis), norm(cCT1_bis) ])).T
+Ytarget_bis=(np.array([np.array(databis['isBB'],dtype=float)])).T
 
 
 ###FUNCTIONS_____________________________________________________________________________________________
@@ -38,7 +42,7 @@ def print_results(inputs: Tensor, targets: Tensor, predictions: Tensor) -> None:
     for x, y, z in zip(inputs, targets, predictions):
         print(f'{x} =>   {y}    => {z.round(5)} =>  {z.round()}') #{float(z):.5f} =>    {z.round()}')
         
-def train_NN(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 1000, loss_func: Loss = MeanSquareError() ):
+def train_NN(net: Optimizer, inputs: Tensor, targets: Tensor, loss_func: Loss = MeanSquareError(), epochs: int = 10000):
     train(net, inputs, targets, num_epochs=epochs, loss=loss_func)
     predictions = net.forward(inputs)
     print_results(inputs, targets, predictions)
@@ -48,7 +52,8 @@ def train_NN(net: Optimizer, inputs: Tensor, targets: Tensor, epochs: int = 1000
 
 def test_NN(net: Optimizer, inputs: Tensor, targets: Tensor, loss_func: Loss = MeanSquareError()):
     predictions = net.forward(inputs)
-    print_results(inputs, targets, predictions)    
+    print_results(inputs, targets, predictions)
+    
     Loss_value=loss_func.loss(predictions,targets)
     print("\nloss function:", Loss_value)
 
@@ -60,13 +65,14 @@ L_out=Ytarget.shape[1]
 
 net = NeuralNet([
         # Add the layers here
-        Linear(input_size=L_in, output_size=L_in),
-        Tanh(),
+        Linear(input_size=L_in, output_size=25),
+        #Tanh(),
+        #Linear(input_size=L_in, output_size=L_in),
         #ReLu(),
-        #Sigmoid(),
-        Linear(input_size=L_in, output_size=L_out)
+        Sigmoid(),
+        Linear(input_size=25, output_size=L_out)
 ])
 
 train_NN(net, X, Ytarget)
 #print("\n\n\n")
-#test_NN(net, X2, Ytarget2)
+#test_NN(net, X_bis, Ytarget_bis)
