e5a3fbd13b583deee371482aadd3ef08da406d3e
SachaCorm <60102528+SachaCorm@users.noreply.github.com>
2020-02-23 23:55:27 +0100

    deleting over functions, to put in another file

--- hist_plot.py
+++ hist_plot.py
@@ -1,21 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from Sidefunc import *
 from DataReader import *
-
-def str2tuple(data, colonne):
-    return list(map(float,data[colonne])), list(map(float,data['isBB']))  #Take data from a chosen column of the file. Convert it in floats.
-
-def sort(data, result):
-    bb=[]
-    nobb=[]
-    for i in range (0,len(data)):               #Fill a tuple with bbar particles, and another with no bbar particles
-        if result[i]==1:
-            bb.append(data[i])
-        else :
-            nobb.append(data[i])
-    return (bb, nobb)
-
     
 def data_hist(bb, nobb, name):                  #Plot an histogram for bbar particles and no bbar particles
     plt.hist(nobb, bins=50, label='nobb', edgecolor='r', alpha=0.5)
